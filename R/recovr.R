031e2f17b8c55597ef31bb462c1632a0d62bf059
Jonathan McPherson <jonathan@rstudio.com>
2019-11-18 16:00:24 -0800

    use rprojroot to find the project root

--- R/recovr.R
+++ R/recovr.R
@@ -13,8 +13,6 @@
 #'
 #' @return A data frame listing the recovered files.
 #'
-#' @export
-#'
 #' @examples
 #' \dontrun{
 #' # Recover files in the current project to the temporary directory
@@ -24,12 +22,23 @@
 #' recovr(project_path = "all", out_folder = "~/scratch/rstudio")
 #'
 #' }
+#'
+#' @export
+
 recovr <- function(project_path,
                    out_folder = file.path(tempdir(), "rsrecovr"),
                    force = FALSE) {
   if (missing(project_path)) {
     # user has not specified a project path; try to infer from working dir
-    project_path <- here::here()
+    tryCatch({
+      project_path <- rprojroot::find_root(rprojroot::is_rstudio_project)
+    }, error = function(e) {
+      warning("The current path (", getwd(), ") does not appear to be inside ",
+              "an RStudio project. Files not open in a project will be ",
+              "recovered. Suppress this warning with project_path = NULL ",
+              "to explicitly restore files not open in a project.")
+      project_path <<- NULL
+    })
   }
 
   # establish output folder
